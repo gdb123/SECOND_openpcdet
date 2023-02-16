64635658bb572fab953a82d43b2a81b4c3d7f630
gdb123 <72054354+gdb123@users.noreply.github.com>
2023-02-16 10:20:27 +0800

    Update mean_vfe.py

--- pcdet/models/backbones_3d/vfe/mean_vfe.py
+++ pcdet/models/backbones_3d/vfe/mean_vfe.py
@@ -6,6 +6,7 @@ from .vfe_template import VFETemplate
 class MeanVFE(VFETemplate):
     def __init__(self, model_cfg, num_point_features, **kwargs):
         super().__init__(model_cfg=model_cfg)
+       # 每个点多少个特征(x，y，z，r)
         self.num_point_features = num_point_features
 
     def get_output_feature_dim(self):
@@ -23,9 +24,14 @@ class MeanVFE(VFETemplate):
             vfe_features: (num_voxels, C)
         """
         voxel_features, voxel_num_points = batch_dict['voxels'], batch_dict['voxel_num_points']
-        points_mean = voxel_features[:, :, :].sum(dim=1, keepdim=False)
+       # 求每个voxel内 所有点的和       
+       # eg:SECOND  shape (Batch*16000, 5, 4) -> (Batch*16000, 4)
+       points_mean = voxel_features[:, :, :].sum(dim=1, keepdim=False)
+       # 正则化项， 保证每个voxel中最少有一个点，防止除
         normalizer = torch.clamp_min(voxel_num_points.view(-1, 1), min=1.0).type_as(voxel_features)
-        points_mean = points_mean / normalizer
+        # 求每个voxel内点坐标的平均
+       points_mean = points_mean / normalizer
+      # 将处理好的voxel_feature信息重新加入batch_di
         batch_dict['voxel_features'] = points_mean.contiguous()
 
         return batch_dict
